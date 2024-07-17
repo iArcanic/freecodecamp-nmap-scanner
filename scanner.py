7682c28b2d8946fb4b5a361392203804dbd68737
Preetham Ananthkumar <preetham.mervin@gmail.com>
2024-07-17 13:10:56 +0100

    Added comprehensive TCP scan functionality

--- scanner.py
+++ scanner.py
@@ -25,7 +25,7 @@ if resp == 1:
     # Nmap scanner version
     print(f"Nmap Version: {scanner.nmap_version()}")
 
-    # TCP scan from ports 1-1024 and verbose mode
+    # Scan from ports 1-1024, verbose mode (-v), TCP SYN ACK scan (-sS)
     scanner.scan(ip_addr, "1-1024", "-v", "-sS")
     print(scanner.scaninfo())
 
@@ -41,7 +41,7 @@ elif resp == 2:
     # Nmap scanner version
     print(f"Nmap Version: {scanner.nmap_version()}")
 
-    # UDP scan from ports 1-1024 and verbose mode
+    # From ports 1-1024, verbose mode (-v), UDP scan (-sU)
     scanner.scan(ip_addr, "1-1024", "-v", "-sU")
     print(scanner.scaninfo())
 
@@ -53,6 +53,22 @@ elif resp == 2:
 
     # All open UDP ports
     print(f"Open Ports: {scanner[ip_addr]["udp"].keys()}")
+elif resp == 3:
+    # Nmap scanner version
+    print(f"Nmap Version: {scanner.nmap_version()}")
+
+    # Scan from ports 1-1024, verbose mode (-v), TCP SYN ACK scan (-sS), service enumeration (-sV), default scripts (-sC), aggressive mode (-A), operating system scan (-O)
+    scanner.scan(ip_addr, "1-1024", "-v", "-sS -sV -sC -A -O")
+    print(scanner.scaninfo())
+
+    # IP address status
+    print(f"IP Status: {scanner[ip_addr].state()}")
+
+    # All available network protocols
+    print(f"Protocols: {scanner[ip_addr].all_protocols()}")
+
+    # All open UDP ports
+    print(f"Open Ports: {scanner[ip_addr]["tcp"].keys()}")
 else:
     # If user enters a number that isn't a valid option
     print("Invalid option selected! Please re-run the program again!")
